7e54628cbadfe207e73a60cc28752fccb8bcb83b
Samuel Kim <samuelkyuekim@gmail.com>
2023-07-27 15:41:41 -0400

    changed git directory from original researchers and checking data with print

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -14,15 +14,16 @@ _SCORE_MAP = {
 class Analysis(object):
     def __init__(self,):
         return
-    
+
     def get_verbosity(self,data):
         data['answer'] = data['answer'].astype(str)
+        print(data['answer'][0])
         data['verbosity'] = data['answer'].apply(len)
         average_lengths = data.groupby('model')['verbosity'].mean()
         scores_std = data.groupby('model')['verbosity'].std(ddof=0) / numpy.sqrt(data.groupby('model').size())
 
         return average_lengths, scores_std
-    
+
     def get_score(self,data,name):
         if name in _SCORE_MAP:
             method = _SCORE_MAP[name]
@@ -31,23 +32,23 @@ class Analysis(object):
             scores = data.groupby('model')[name].mean()
             scores_std = data.groupby('model')[name].std(ddof=0) / numpy.sqrt(data.groupby('model').size())
             return scores, scores_std
-    
-    def get_overlap(self,data,models,name):  
+
+    def get_overlap(self,data,models,name):
         filtered_df1 = data[data['model']==models[0]]
         filtered_df2 = data[data['model']==models[1]]
         merged_df = pandas.merge(filtered_df1, filtered_df2, on='id')
-        merged_df['Answer Overlap'] = merged_df.apply(lambda row: match_score(row, name=name,), axis=1)  
+        merged_df['Answer Overlap'] = merged_df.apply(lambda row: match_score(row, name=name,), axis=1)
         scores = merged_df['Answer Overlap'].mean()
-        
+
         scores_std = merged_df['Answer Overlap'].std(ddof=0) / numpy.sqrt(merged_df['Answer Overlap'].size)
 
         return scores, scores_std
-        
+
     def get_code(self,data,name):
         if name in _SCORE_MAP:
             method = _SCORE_MAP[name]
             data[name] = data.apply(method,axis=1)
             #scores = data.groupby('model')[name].mean()
             #scores_std = data.groupby('model')[name].std()
-            return #scores, scores_std        
-        
+            return #scores, scores_std
+
